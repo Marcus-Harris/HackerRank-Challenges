90bd6d26494c7545d0229bae125abc825de71f1d
Marcus Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-04-22 17:24:05 -0400

    Update JavaLoops.java

--- JavaLoops.java
+++ JavaLoops.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class JavaLoops {
     Scanner scanner = new Scanner(System.in);
     public void loops() {
+        System.out.println("Enter a number and I will multiply it by every number between 1 and 10 (inclusive).");
         int N = scanner.nextInt();
         int i;
         for (i = 1; i <= 10; i++) {
