eb1909672dcecaa73ac84e7ea611dc114e84abb1
Marcus Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-04-21 20:27:24 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ package com.tts;
 
 public class Main {
     public static void main(String[] args) {
+
+        Welcome welcome = new Welcome();
+        welcome.welcomeToJava();
+
         Java_If_Else newIfElse = new Java_If_Else();
         newIfElse.ifElse();
     }
